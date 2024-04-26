f427d86452490d1ec812f4ed10a7cffa9e510769
JeoffMinas <168161024+JeoffMinas@users.noreply.github.com>
2024-04-26 13:06:19 +0800

    Update array-challenge.js

--- array-challenge.js
+++ array-challenge.js
@@ -15,3 +15,19 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     7
     9
 */
+    //Answer
+let evens = "";
+let odds = "";
+
+for (let num of arr) {
+  if (num % 2 === 0) {
+    evens += num + "\n";
+  } else {
+    odds += num + "\n";
+  }
+}
+
+console.log("EVEN NUMBERS:");
+console.log(evens);
+console.log("ODD NUMBERS:");
+console.log(odds);
